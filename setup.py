5aaac66618d676f99747f4bc0d7d4c2690af86b1
Chris O'Connor <45886288+cdoconno@users.noreply.github.com>
2020-12-12 11:46:12 -0600

    Reconfigured setup.py to satisfy requirements in #7

--- setup.py
+++ setup.py
@@ -1,22 +1,50 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def get_readme_md_contents():
+    with open('README.md', 'r') as f:
+        desc = f.read()
+        return desc
+
 
 setuptools.setup(
     name="QCluster",
     version="0.0.1",
-    author="Aaron Rohrbaugh, Katie Pikturna",
-    author_email="aaronr@vt.edu, pkat22@vt.edu",
+    author="Aaron Rohrbaugh + (See GitHub)",
+    author_email="chriso@qsonlabs.com",
+    maintainer="Chris O'Connor",
+    maintainer_email="chriso@qsonlabs.com",
     description="An SDK to enable quick clustering of microservices.",
-    long_description=long_description,
+    license="Apache License 2.0",
+    long_description=get_readme_md_contents(),
     long_description_content_type="text/markdown",
-    url="https://github.com/....",
+    url="https://github.com/QsonLabs/qcluster",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Apache-2.0",
-        "Operating System :: OS Independent",
+    test_suite="tests",
+    tests_require=[
+        "flake8",
+        "pytest",
+        "pytest-asyncio",
+        "pytest-cov",
+        "tox"
     ],
-    python_requires='>=3.6',
+    install_requires=[
+        "aiohttp"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Flake8",
+        "Framework :: Pytest",
+        "Framework :: tox",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Clustering",
+        "Topic :: System :: Distributed Computing",
+    ],
+    python_requires='>=3.8',
 )
